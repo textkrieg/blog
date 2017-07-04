627136d031487429549a7c596568fee3d13f6902
Johannes 'fish' Ziemke <github@freigeist.org>
2017-07-04 20:18:44 +0200

    Add links to books and freelance

--- components/SiteNav/index.jsx
+++ components/SiteNav/index.jsx
@@ -13,11 +13,15 @@ class SiteNav extends React.Component {
             </Link>
           </li>
           <li>
-            <Link to={prefixLink('/about/')} activeClassName="current"> About me
+            <Link to={prefixLink('/books/')} activeClassName="current"> Books
             </Link>
           </li>
           <li>
-            <Link to={prefixLink('/contact/')} activeClassName="current"> Contact me
+            <Link to={prefixLink('/freelance/')} activeClassName="current"> Freelance
+            </Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/contact/')} activeClassName="current"> Contact
             </Link>
           </li>
         </ul>
